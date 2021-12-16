50a910ebb98eecc328653b180ada536613d6d01b
Jaeyoung Park <52947910+jaeyoung011@users.noreply.github.com>
2021-12-16 11:01:23 +0900

    Delete replace-employee-id-with-the-unique-identifier directory

--- replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
+++
@@ -1,8 +0,0 @@
-# Write your MySQL query statement below
-
-
-SELECT CASE WHEN U.unique_id IS NULL THEN null
-            WHEN U.unique_id IS NOT NULL THEN unique_id
-        END AS unique_id, E.name 
-FROM Employees E
-LEFT JOIN EmployeeUNI U on E.id = U.id
