51faa0ce16f29ecfffc5521172956cdca8b99af5
Jaeyoung Park <52947910+jaeyoung011@users.noreply.github.com>
2021-12-16 11:06:01 +0900

    Update replace-employee-id-with-the-unique-identifier.sql

--- replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
+++ replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
@@ -7,7 +7,5 @@ SELECT CASE WHEN U.unique_id IS NULL THEN null
 FROM Employees E
 LEFT JOIN EmployeeUNI U on E.id = U.id
 
+# LEFT JOIN , RIGHT JOIN 기준이 되는 컬럼에 따라 값이 달라진다.
 
-```
-LEFT JOIN , RIGHT JOIN 기준이 되는 컬럼에 따라 값이 달라진다.
-```
