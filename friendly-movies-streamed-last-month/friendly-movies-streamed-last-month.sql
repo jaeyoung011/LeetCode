8a99b3fd413ac883acd1652dc616f57107af012b
Jaeyoung Park <52947910+jaeyoung011@users.noreply.github.com>
2021-12-17 10:18:31 +0900

    Update friendly-movies-streamed-last-month.sql

--- friendly-movies-streamed-last-month/friendly-movies-streamed-last-month.sql
+++ friendly-movies-streamed-last-month/friendly-movies-streamed-last-month.sql
@@ -7,3 +7,5 @@ WHERE month(program_date) = 6
         and year(program_date) = 2020
         and kids_content = 'Y'
         and content_type = 'Movies'
+        
+ # 처음에 year 그리고 content_type 을 빼먹고 작성을 했다.
